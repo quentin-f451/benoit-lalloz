0018b891770f45212566a992fc338644becba3f8
quentin-f451 <quentin@f451.faith>
2019-04-10 20:14:17 +0200

    Content

--- site/plugins/feed/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Kirby::plugin('quentincrzt/feed', [
-    'fields' => array(
-        'feed' => require_once __DIR__ . '/lib/feed.php',
-    )
-]);
-
-
--- site/plugins/methods/index.php
+++ site/plugins/methods/index.php
@@ -5,63 +5,63 @@ Kirby::plugin('quentin-f451/methods', [
         'toCategory' => function($field) {
           $value = $field->value;
           $splittedValues = explode(", ", $value);
-		  $allValues = "";
-		  
-          foreach ($splittedValues as $val) :
-            $allValues .= '<span class="category-label" data-category="'. $val .'">' . $val . '</span>';
-		  endforeach;
-		  
-          return $allValues;
-		},
-		
-		'toCaption' => function ($field, $feed) {
-			$value = $field->value;
-			$projet = $field->model();
-			$ret = "";
-
-			if ($value == true) {
-				$types = explode(", ", $projet->categorie());
-
-				if ($feed == 'home') {
-					$i = 0;
-					foreach ($types as $t):
-						$i++;
-						if ($t == 'collections' && !empty($types)) {
-							if ($projet->titleInCollection() != '') {
-								$ret .= 'In ' . $projet->collection() . ' collection, ' . $projet->titleInCollection();
-							} else {
-								$ret .= 'In ' . $projet->collection() . ' collection, ' . $projet->title();
-							}
-						} else if ($t == 'spaces' && !empty($types)) {
-							$ret .= 'Spaces for ' . $projet->client() . ' in ' . $projet->lieu();
-						} else if ($t == 'laboratory' && !empty($types)) {
-							$ret .= 'From our Laboratory, watch ' . $projet->title();
-						}
-						if($i < count($types))
-							$ret .= " and ";
-						else
-							$ret .= ".";
+					$allValues = "";
+					
+					foreach ($splittedValues as $val) :
+						$allValues .= '<span class="category-label" data-category="'. $val .'">' . $val . '</span>';
 					endforeach;
-				} else {
-					if (in_array('collections', $types) && $feed == 'collections' && !empty($types)) {
-						if ($projet->titleInCollection() != '') {
-							$ret = 'In ' . $projet->collection() . ' collection, ' . $projet->titleInCollection();
+					
+					return $allValues;
+				},
+				
+				'toCaption' => function ($field, $feed) {
+					$value = $field->value;
+					$projet = $field->model();
+					$ret = "";
+
+					if ($value == true) {
+						$types = explode(", ", $projet->categorie());
+
+						if ($feed == 'home') {
+							$i = 0;
+							foreach ($types as $t):
+								$i++;
+								if ($t == 'collections' && !empty($types)) {
+									if ($projet->titleInCollection() != '') {
+										$ret .= 'In ' . $projet->collection() . ' collection, ' . $projet->titleInCollection();
+									} else {
+										$ret .= 'In ' . $projet->collection() . ' collection, ' . $projet->title();
+									}
+								} else if ($t == 'spaces' && !empty($types)) {
+									$ret .= 'Spaces for ' . $projet->client() . ' in ' . $projet->lieu();
+								} else if ($t == 'laboratory' && !empty($types)) {
+									$ret .= 'From our Laboratory, watch ' . $projet->title();
+								}
+								if($i < count($types))
+									$ret .= " and ";
+								else
+									$ret .= ".";
+							endforeach;
 						} else {
-							$ret = 'In ' . $projet->collection() . ' collection, ' . $projet->title();
+							if (in_array('collections', $types) && $feed == 'collections' && !empty($types)) {
+								if ($projet->titleInCollection() != '') {
+									$ret = 'In ' . $projet->collection() . ' collection, ' . $projet->titleInCollection();
+								} else {
+									$ret = 'In ' . $projet->collection() . ' collection, ' . $projet->title();
+								}
+							} else if (in_array('spaces', $types) && $feed == 'spaces' && !empty($types)) {
+								$ret = 'Spaces for ' . $projet->client() . ' in ' . $projet->lieu();
+							} else if (in_array('laboratory', $types) && $feed == 'laboratory' && !empty($types)) {
+								$ret = 'From our Laboratory, watch ' . $projet->title();
+							}
+							$ret .= ".";
 						}
-					} else if (in_array('spaces', $types) && $feed == 'spaces' && !empty($types)) {
-						$ret = 'Spaces for ' . $projet->client() . ' in ' . $projet->lieu();
-					} else if (in_array('laboratory', $types) && $feed == 'laboratory' && !empty($types)) {
-						$ret = 'From our Laboratory, watch ' . $projet->title();
+
+					} else {
+						$ret = $projet->legendeManuelle();
 					}
-					$ret .= ".";
+
+					return $ret;
 				}
-
-			} else {
-				$ret = $projet->legendeManuelle();
-			}
-
-      return $ret;
-		}
-	]
+		]
 ]);
